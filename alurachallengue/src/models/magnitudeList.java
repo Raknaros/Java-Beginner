f6bde9fa40e59c5bdbd9d34ae1e879cd786d2bb1
Raknaros <giumarchand@gmail.com>
2023-04-01 01:40:25 -0500

    Prerelease

--- alurachallengue/src/models/magnitudeList.java
+++ alurachallengue/src/models/magnitudeList.java
@@ -5,25 +5,8 @@ import java.util.ArrayList;
 import java.util.HashMap;
 
 public class magnitudeList {
-    public static ArrayList<HashMap> magnitudes=new ArrayList<>();
-    public static HashMap<String, BigDecimal> longitud=new HashMap<>();
-    public static HashMap<String, BigDecimal> masa=new HashMap<>();
-    public static HashMap<String, BigDecimal> temperatura=new HashMap<>();
+    public String [][] longitud={{"Kilometros","Yardas","Millas"},{"1000","0.9144","1609.34"}},
+    masa={{"Kilogramos","Libras","Onzas"},{"1000","453.59","28.70"}},
+    temperatura={{"Kelvin","Celcius","Farenheit"},{"373.15","100","212"}};
 
-    public static void main(String[] args) {
-
-        longitud.put("Kilometros",new BigDecimal(1000));
-        longitud.put("Yardas",new BigDecimal(0.9144));
-        longitud.put("Millas",new BigDecimal(1609.34));
-        masa.put("Kilogramos",new BigDecimal(1000));
-        masa.put("Libras",new BigDecimal(453.59));
-        masa.put("Onzas",new BigDecimal(28.70));
-        temperatura.put("Kelvin",new BigDecimal(373.15));
-        temperatura.put("Celcius",new BigDecimal(100));
-        temperatura.put("Farenheit",new BigDecimal(212));
-        magnitudes.add(longitud);
-        magnitudes.add(masa);
-        magnitudes.add(temperatura);
-
-    }
 }
--- alurachallengue/src/models/scrapCurrency.java
+++ alurachallengue/src/models/scrapCurrency.java
@@ -5,6 +5,7 @@ import org.jsoup.select.*;
 import java.io.IOException;
 import java.math.BigDecimal;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -12,8 +13,9 @@ import static java.lang.Double.parseDouble;
 
 
 public class scrapCurrency {
-    private static Map<String, BigDecimal> rateMap=new HashMap<>();
-    private static String[] currencies={"Pesos Argentinos","Bolivianos","Reales","Pesos Chilenos","Pesos Colombianos","Guaranis","Soles","Pesos Uruguayos","Bolivares"};
+    public static String[][] currenciesRates=new String[1][8];
+    public static String[] currencies={"Pesos Argentinos","Bolivianos","Reales","Pesos Chilenos","Pesos Colombianos","Guaranis","Soles","Pesos Uruguayos","Bolivares"};
+    public static String[] rates=new String[9];
     static int c=0;
     static int j=0;
     public static void main(String[] args) throws IOException {
@@ -23,13 +25,17 @@ public class scrapCurrency {
         for(Element i:exchange){
             if(!i.ownText().isEmpty()){
                 if (c%2==0){
-                    scrapCurrency.rateMap.put(currencies[j],new BigDecimal(parseDouble(i.ownText().replace(",",""))));
+                    rates[j]=i.ownText().replace(",","");
+                    //scrapCurrency.rateMap.put(currencies[j],new BigDecimal(parseDouble(i.ownText().replace(",",""))));
+                    System.out.println(rates[j]);
                     j++;
                 }
 
                 c++;
             }
         }
+        currenciesRates[0]= new String[]{Arrays.toString(currencies)};
+        currenciesRates[1]= new String[]{Arrays.toString(rates)};
     }
 }
 
